7671815a76a1d57da7578450af29d6ee0c56c2e5
Hakuryu Yoshino <whitedragon7980@gmail.com>
2014-07-11 11:55:05 +0900

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+    
+   get 'users/new'
 
-  get 'user/new'
+    get 'user/new'
   
     resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+   match '/signup',  to: 'users#new',            via: 'get'
+   match '/signin',  to: 'sessions#new',         via: 'get'
+   match '/signout', to: 'sessions#destroy',     via: 'delete'
+      
     root  'static_pages#home'
     match '/help',    to: 'static_pages#help',    via: 'get'
     match '/about',   to: 'static_pages#about',   via: 'get'
